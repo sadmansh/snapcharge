fdf5a0ba56a3a47dc1b22bdfeaf556280db2a4af
Sadman Shawmik <hello@sadmansh.com>
2020-05-07 03:04:53 +0600

    Prevent duplicate customer creation

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -6,30 +6,34 @@ const mongoose = require('mongoose')
 const Customer = mongoose.model('customers')
 
 router.post('/customers/create', passport.authenticate('jwt', { session: false }), async (req, res) => {
+
 	try {
-		const customer = await stripe.customers.create({
-			email: req.body.email,
-			name: req.body.name,
-			metadata: {
-				user: req.user.id
-			},
-		})
-		if (customer) {
-			try {
-				const existingCustomer = await Customer.findOne({ email: customer.email, _user: req.user.id })
-				if (existingCustomer) return res.status(200).send({ error: 'Customer already exists.' })
-				const newCustomer = await new Customer({
-					stripeId: customer.id,
-					_user: req.user.id,
-					name: customer.name,
-					email: customer.email,
-					created: customer.created,
-					currency: customer.currency
-				}).save()
-				res.send(newCustomer)
-			} catch (error) {
-				return res.status(200).send({ error: error.message })
+		const existingCustomer = await Customer.findOne({ email: req.body.email, _user: req.user.id })
+		if (!existingCustomer) {
+			const customer = await stripe.customers.create({
+				email: req.body.email,
+				name: req.body.name,
+				metadata: {
+					user: req.user.id
+				},
+			})
+			if (customer) {
+				try {
+					const newCustomer = await new Customer({
+						stripeId: customer.id,
+						_user: req.user.id,
+						name: customer.name,
+						email: customer.email,
+						created: customer.created,
+						currency: customer.currency
+					}).save()
+					res.send(newCustomer)
+				} catch (error) {
+					return res.status(200).send({ error: error.message })
+				}
 			}
+		} else {
+			return res.status(200).send({ error: 'Customer already exists.' })
 		}
 	} catch (error) {
 		res.status(200).send({ error: error.message })
