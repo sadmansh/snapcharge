5abd23be70155c4b8bc29c967783cd7300da2f94
Sadman Shawmik <hello@sadmansh.com>
2020-05-23 04:33:19 +0600

    Add antd to create customer form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import * as actions from '../actions'
+import 'antd/dist/antd.css' 
 
 import Landing from './Landing'
 import Login from './Login'
--- client/src/components/Customers/CreateCustomer.js
+++ client/src/components/Customers/CreateCustomer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../../actions'
+import { Form, Input, Select, Button } from 'antd'
+
+const { Option } = Select
 
 class CreateCustomer extends Component {
 	state = {
@@ -9,25 +12,49 @@ class CreateCustomer extends Component {
 		currency: ''
 	}
 
-	handleChange = (e) => {
-		const { name, value } = e.target
-		this.setState({ [name]: value })
-	}
-
-	handleSubmit = (e) => {
-		e.preventDefault()
-		const customer = this.state
+	handleSubmit = (customer) => {
 		if (customer) this.props.createCustomer(customer)
 	}
 
 	render() {
+		const layout = {
+			labelCol: { span: 2},
+			wrapperCol: { span: 4 }
+		}
+
+		const buttonLayout = {
+			wrapperCol: { offset: 2, span: 4 }
+		}
+
+		const currencyOptions = [
+			{ key: 'usd', label: 'US Dollar' },
+			{ key: 'eur', label: 'Euro' },
+			{ key: 'aud', label: 'Australian Dollar' },
+			{ key: 'gbp', label: 'British Pound' }
+		]
+
 		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<input type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange} />
-					<input type="email" name="email" placeholder="Email address" value={this.state.email} onChange={this.handleChange} />
-					<button type="submit">Create customer</button>
-				</form>
+			<div className="CreateCustomer">
+				<Form {...layout} onFinish={this.handleSubmit}>
+					<Form.Item name="name" label="Name" rules={[{ required: true }]}>
+						<Input />
+					</Form.Item>
+					<Form.Item name="email" label="Email" rules={[{ required: true }]}>
+						<Input />
+					</Form.Item>
+					<Form.Item name="currency" label="Currency" rules={[{ required: true }]}>
+						<Select>
+							{currencyOptions.map(({ key, label }) => {
+								return (
+									<Option key={key} value={key}>{`${key.toUpperCase()} - ${label}`}</Option>
+								)
+							})}
+						</Select>
+					</Form.Item>
+					<Form.Item {...buttonLayout}>
+						<Button type="primary" htmlType="submit">Add customer</Button>
+					</Form.Item>
+				</Form>
 			</div>
 		) 
 	}
