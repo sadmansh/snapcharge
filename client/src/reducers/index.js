2fc027de121f49c4f2dcb7cde239f0d2039fa420
Sadman Shawmik <hello@sadmansh.com>
2020-05-22 22:58:43 +0600

    Add redux form reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from './authReducer'
 import countriesReducer from './countriesReducer'
 import customerReducer from './customerReducer'
 import invoiceReducer from './invoiceReducer'
+import { reducer as formReducer } from 'redux-form'
 
 export default combineReducers({
 	user: authReducer,
 	countries: countriesReducer,
 	customers: customerReducer,
-	invoices: invoiceReducer
+	invoices: invoiceReducer,
+	form: formReducer
 })
