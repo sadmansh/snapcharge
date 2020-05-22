305a97c0d3985a014e18f8fa754936cad809cb4b
Sadman Shawmik <hello@sadmansh.com>
2020-05-22 22:55:00 +0600

    Untrack keys

--- server/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	mongoURI: 'mongodb+srv://snapcharge-dev-user:bFR5x3gNXAFFhJ4Q@snapcharge-dev-fad2t.mongodb.net/dev',
-	cookieKey: 'idealismsitsinPrison@',
-	redirectDomain: 'http://localhost:3000',
-	jwtSecret: 'Chivalryfellonitssword!',
-	stripeSecretKey: 'sk_test_oqFe3IVNcOGwMnrjjy7Qf1oV00ZdvWdPKF',
-}
