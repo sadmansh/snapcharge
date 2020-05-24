0e68870c634818afb6946b201cad97b3f47493dc
Sadman Shawmik <hello@sadmansh.com>
2020-05-24 08:24:02 +0600

    Add ant layout and menu to dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
 import * as actions from '../actions'
+import { Layout } from 'antd'
+import './Dashboard.scss'
 
 import DashboardNavigation from './DashboardNavigation'
 import Customers from './Customers/Customers'
@@ -9,17 +11,26 @@ import CustomerDetails from './Customers/CustomerDetails'
 import Invoices from './Invoices/Invoices'
 import CreateInvoice from './Invoices/CreateInvoice'
 
+const { Content, Sider } = Layout
+
 class Dashboard extends Component {
 	
 	render() {
 		return (
-			<div>
-				<DashboardNavigation />
-				<h1>Hello, {this.props.user.firstName}</h1>
-				<Route exact path="/dashboard/customers" component={Customers} />
-				<Route path="/dashboard/customers/:stripeId" component={CustomerDetails} />
-				<Route exact path="/dashboard/invoices" component={Invoices} />
-				<Route exact path="/dashboard/invoices/create" component={CreateInvoice} />
+			<div style={{ background: '#e6eaef' }}>
+				<Layout style={{ padding: '2rem 0', background: 'transparent', maxWidth: '1360px', marginLeft: 'auto', marginRight: 'auto' }}>
+					<Sider style={{ height: '100vh', background: 'transparent' }}>
+						<DashboardNavigation />						
+					</Sider>
+					<Content style={{ padding: '2rem 4rem' }}>
+						<div className="dashboard-content">
+							<Route exact path="/dashboard/customers" component={Customers} />
+							<Route path="/dashboard/customers/:stripeId" component={CustomerDetails} />
+							<Route exact path="/dashboard/invoices" component={Invoices} />
+							<Route exact path="/dashboard/invoices/create" component={CreateInvoice} />
+						</div>
+					</Content>
+				</Layout>
 			</div>
 		)
 	}
--- client/src/components/DashboardNavigation.js
+++ client/src/components/DashboardNavigation.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import * as actions from '../actions'
+import { Menu } from 'antd'
+import { UserOutlined, ContainerOutlined } from '@ant-design/icons'
 
 class DashboardNavigation extends Component {
 	render() {
 		return (
-			<nav>
-				<Link to="/dashboard/customers">Customers</Link>
-				<Link to="/dashboard/invoices">Invoices</Link>
+			<nav className="dashboard-navigation" style={{ height: '100%' }}>
+				<Menu mode="vertical" style={{ height: '100%', padding: '2rem 0', border: 'none', background: 'transparent' }}>
+					<Menu.Item key="customers" icon={<UserOutlined />}>
+						<Link to="/dashboard/customers">Customers</Link>
+					</Menu.Item>
+					<Menu.Item key="invoices" icon={<ContainerOutlined />}>
+						<Link to="/dashboard/invoices">Invoices</Link>
+					</Menu.Item>
+				</Menu>
 			</nav>
 		)
 	}
