59fc131ff214ce9f10a77b12c4f651c96c3b2803
Sadman Shawmik <hello@sadmansh.com>
2020-05-29 21:01:14 +0600

    Fix invoice detail component appearing in invoice create

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import * as actions from '../../actions'
 import { Layout } from 'antd'
 import './Dashboard.scss'
@@ -27,13 +27,15 @@ class Dashboard extends Component {
 					</Sider>
 					<Content style={{ padding: '4rem' }}>
 						<div className="dashboard-content">
-							<Route exact path="/dashboard" component={DashboardView} />
-							<Route exact path="/dashboard/payments" component={Payments} />
-							<Route exact path="/dashboard/customers" component={Customers} />
-							<Route path="/dashboard/customers/:id" component={CustomerDetails} />
-							<Route exact path="/dashboard/invoices" component={Invoices} />
-							<Route path="/dashboard/invoices/:id" component={InvoiceDetails} />
-							<Route exact path="/dashboard/invoices/create" component={CreateInvoice} />
+							<Switch>
+								<Route exact path="/dashboard" component={DashboardView} />
+								<Route exact path="/dashboard/payments" component={Payments} />
+								<Route exact path="/dashboard/customers" component={Customers} />
+								<Route path="/dashboard/customers/:id" component={CustomerDetails} />
+								<Route exact path="/dashboard/invoices" component={Invoices} />
+								<Route exact path="/dashboard/invoices/create" component={CreateInvoice} />
+								<Route path="/dashboard/invoices/:id" component={InvoiceDetails} />
+							</Switch>
 						</div>
 					</Content>
 				</Layout>
