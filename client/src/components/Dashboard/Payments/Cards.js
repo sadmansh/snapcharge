524986351cd6457ac0742db6d36590696bcb6a82
Sadman Shawmik <hello@sadmansh.com>
2020-06-24 01:48:01 +0600

    Display current & unpaid balance

--- client/src/components/Dashboard/Payments/Cards.js
+++ client/src/components/Dashboard/Payments/Cards.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
+import getCurrencySymbol from '../../../utils/getCurrencySymbol'
 
 class Cards extends Component {
+
 	render() {
 		return (
 			<div className="payment-cards">
 				<div className="card" style={{ backgroundImage: 'linear-gradient(to right, #5f70e3, #8161e3)' }}>
 					<span className="card-title">Current balance</span>
-					<span className="card-data">$5600</span>
+					<span className="card-data">{getCurrencySymbol(this.props.currency)}{(this.props.data.received / 100).toFixed(2)}</span>
 				</div>
 				<div className="card" style={{ backgroundImage: 'linear-gradient(to right, #f53859, #f5593a)' }}>
 					<span className="card-title">Yet to receive</span>
-					<span className="card-data">$5600</span>
+					<span className="card-data">{getCurrencySymbol(this.props.currency)}{(this.props.data.unpaid / 100).toFixed(2)}</span>
 				</div>
 				<div className="card" style={{ backgroundImage: 'linear-gradient(to right, #0fcaef, #0e75ef)' }}>
 					<span className="card-title">Last Payout</span>
--- client/src/components/Dashboard/Payments/Payments.js
+++ client/src/components/Dashboard/Payments/Payments.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as actions from '../../../actions'
 import Cards from './Cards'
 import './Payments.scss'
 
 class Payments extends Component {
+
+	componentDidMount() {
+		this.props.fetchPaymentData()
+	}
+
 	render() {
 		return (
 			<div className="dashboard-item payments">
 				<div className="box">
 					<h1>Payments</h1>
-					<Cards />
+					{this.props.payments ? 
+						<Cards currency={this.props.currency} data={this.props.payments} /> : ''
+					}
 				</div>
 			</div>
 		)
 	}
 }
 
-export default Payments
+const mapStateToProps = ({ user: { currency }, payments }) => {
+	return { currency: currency, payments }
+}
+
+export default connect(mapStateToProps, actions)(Payments)
