60299c7e8b6f14fb80b0c90c400e737e8a52bad7
Sadman Shawmik <hello@sadmansh.com>
2020-06-24 01:54:22 +0600

    Refactor payment data rendering

--- client/src/components/Dashboard/Payments/Cards.js
+++ client/src/components/Dashboard/Payments/Cards.js
@@ -3,16 +3,22 @@ import getCurrencySymbol from '../../../utils/getCurrencySymbol'
 
 class Cards extends Component {
 
+	renderAmount = amount => {
+		if (!amount) amount = 0
+		return getCurrencySymbol(this.props.currency) + (amount / 100).toFixed(2)
+	}
+
 	render() {
+		const { data } = this.props
 		return (
 			<div className="payment-cards">
 				<div className="card" style={{ backgroundImage: 'linear-gradient(to right, #5f70e3, #8161e3)' }}>
 					<span className="card-title">Current balance</span>
-					<span className="card-data">{getCurrencySymbol(this.props.currency)}{(this.props.data.received / 100).toFixed(2)}</span>
+					<span className="card-data">{this.renderAmount(data.received)}</span>
 				</div>
 				<div className="card" style={{ backgroundImage: 'linear-gradient(to right, #f53859, #f5593a)' }}>
 					<span className="card-title">Yet to receive</span>
-					<span className="card-data">{getCurrencySymbol(this.props.currency)}{(this.props.data.unpaid / 100).toFixed(2)}</span>
+					<span className="card-data">{this.renderAmount(data.unpaid)}</span>
 				</div>
 				<div className="card" style={{ backgroundImage: 'linear-gradient(to right, #0fcaef, #0e75ef)' }}>
 					<span className="card-title">Last Payout</span>
