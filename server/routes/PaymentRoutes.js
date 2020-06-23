efceb17a5de38d38695d5715e5aecdb802e62870
Sadman Shawmik <hello@sadmansh.com>
2020-06-24 01:14:50 +0600

    Get aggregate of unpaid payments

--- server/routes/PaymentRoutes.js
+++ server/routes/PaymentRoutes.js
@@ -5,10 +5,25 @@ const stripe = require('stripe')(keys.stripeSecretKey)
 const mongoose = require('mongoose')
 const Payment = mongoose.model('payments')
 
-router.get('/payments/:invoiceId', passport.authenticate('jwt', { session: false }), async (req, res) => {
+router.get('/payments/aggregate', passport.authenticate('jwt', { session: false }), async (req, res) => {
 	try {
-		const payment = await Payment.findOne({ _invoice: req.params.invoiceId })
-		res.send(payment)
+		const totals = await Payment.aggregate([
+		{
+			$match: {
+				_user: req.user._id,
+				status: 'unpaid'
+			}
+		},
+		{
+			$group: {
+				_id: null,
+				unpaid: {
+					$sum: '$subtotal'
+				} 
+			}
+		}
+		])
+		res.send(totals)
 	} catch (error) {
 		res.status(200).send({ error: error.message })
 	}
