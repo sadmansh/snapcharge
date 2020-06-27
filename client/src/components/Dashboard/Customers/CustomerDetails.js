dccf09e74fb6c1d9c5719759fbea2771c5c2a137
Sadman Shawmik <hello@sadmansh.com>
2020-06-27 16:18:07 +0600

    Add basic customer details component

--- client/src/components/Dashboard/Customers/CustomerDetails.js
+++ client/src/components/Dashboard/Customers/CustomerDetails.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import axios from 'axios'
 import AuthHeaders from '../../../utils/AuthHeaders'
+import { Button, Divider, Input, Form } from 'antd'
 
 const CustomerDetails = props => {
 	const [customer, setCustomer] = useState({})
+	const [editMode, setEditMode] = useState(false)
 
 	const fetchCustomer = useCallback(async () => {
-		const customer = await axios.get(`http://localhost:5000/api/customers/${props.match.params.id}`, AuthHeaders)
-		setCustomer(customer.data)
+		const res = await axios.get(`http://localhost:5000/api/customers/${props.match.params.id}`, AuthHeaders)
+		setCustomer(res.data)
 	}, [props.match.params.id])
 
 	useEffect(() => {
@@ -17,13 +19,25 @@ const CustomerDetails = props => {
 	const renderCustomer = () => {
 		if (customer) {
 			return (
-				<div>{customer.name}</div>
+				<div className="box">
+					<div className="section-title">
+						<h1>{customer.name}</h1>
+						<div className="section-actions">
+							<Button type={!editMode ? 'link' : 'primary'} onClick={() => setEditMode(!editMode)}>{!editMode ? 'Edit customer' : 'Save customer'}</Button>
+						</div>
+					</div>
+					<Divider />
+					<div>
+						<label htmlFor="email" style={{ marginBottom: '.5rem', display: 'block', fontWeight: 'bold' }}>Email</label>
+						<span>{customer.email}</span>
+					</div>
+				</div>
 			)
 		}
 	}
 
 	return (
-		<div>{renderCustomer()}</div>
+		<div className="dashboard-item">{renderCustomer()}</div>
 	)
 }
 
